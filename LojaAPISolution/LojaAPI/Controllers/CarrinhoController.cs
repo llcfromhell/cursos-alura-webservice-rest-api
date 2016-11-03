b3cb04771307d8fc6f045c893524508b44531bef
llcfromhell <llc.from.hell@gmail.com>
2016-11-03 08:13:49 -0200

    aula 01

---
+++ LojaAPISolution/LojaAPI/Controllers/CarrinhoController.cs
@@ -0,0 +1,21 @@
+﻿using LojaAPI.DAO;
+using LojaAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LojaAPI.Controllers
+{
+    public class CarrinhoController : ApiController
+    {
+
+        public string Get(int id)
+        {
+            return new CarrinhoDAO().Busca(id).ToXml();
+        }
+
+    }
+}
